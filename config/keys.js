01088d03b2744b67bce84e82f9defb9d58236575
shub12133 <shub.123433@gmail.com>
2020-07-18 18:54:10 +0530

    bugs fix

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,9 @@
 module.exports = {
     mongoURI: "mongodb+srv://shub12144:Shub@12133@cluster0-6pr0w.gcp.mongodb.net/dev?retryWrites=true&w=majority",
-    secretOrKey: 'secret'
+    secretOrKey: 'secret',
+    githubClientId: "c1e597de833c2186c21d",
+	githubSecret: "5c3fd25faa9d4fea252d3bf0e9841ca35e3ee3b6"
+
 };
 
 
