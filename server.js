826148d413712e91de19380c00b282839e17ccac
shub12133 <shub.123433@gmail.com>
2020-07-21 13:53:05 +0530

    bugs

--- server.js
+++ server.js
@@ -26,6 +26,13 @@ app.use(passport.initialize());
 
 // Passport Config
 require('./config/passport')(passport);
+app.use(function (req, res, next){
+   if (req.headers['x-forwarded-proto'] === 'https') {
+     res.redirect('http://' + req.hostname + req.url);
+   } else {
+     next();
+   }
+ });
 
 // Use Routes
 app.use('/api/users', users);
